e937fa2232be9226d04ce4a94478340087f76313
andra manday <andra.manday@gmail.com>
2021-08-19 11:12:15 +0700

    first webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+
+    config.devtool = argv.mode === 'development' ? 'source-map' : false;
+
+    // argv.mode === 'development' ? config.target = 'web'
+    
+    return config;
+
+}
+
+let config = {
+
+    output : {
+        path : path.resolve(__dirname, 'dist')
+    },
+
+    devServer : {
+        port : 3000,
+        open : true,
+        hot  : true
+    },
+
+    plugins : [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template : path.resolve(__dirname, 'src/index.html')
+        })
+    ]
+}
